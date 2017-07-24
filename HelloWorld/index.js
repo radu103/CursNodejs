56378bc549963930c9548e9b5a61b730d840d633
Radu <radu103@hotmail.com>
2017-07-24 11:22:16 +0300

    hello world with Node js server

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,2 +1,27 @@
 /* Hello World! program in Node.js */
-console.log("Hello World!");
+"use strict";
+
+// required module
+var mHttp = require("http");
+
+// variables
+var iPort = 8081;
+var sMessage = "Hello World!";
+
+// create Http Server
+var oServer = mHttp.createServer(function(req, res){
+    console.log("\r\nRequest received");
+   
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    res.write(sMessage);
+
+    res.end();
+
+    console.log("Response sent !");
+});
+
+oServer.listen(iPort);
+
+// Console will print the server info
+console.log('Server running at http://127.0.0.1:' + iPort + '/');
