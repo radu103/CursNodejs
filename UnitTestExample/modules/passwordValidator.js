1da1ee1057bffb8ae68aaee91905010c7533efc5
Radu <radu103@hotmail.com>
2017-07-25 12:31:46 +0300

    function implemented

--- UnitTestExample/modules/passwordValidator.js
+++ UnitTestExample/modules/passwordValidator.js
@@ -1,3 +1,14 @@
 exports.validPassword = module.exports.validPassword = function(password){
+    
+    // test 1 implementation
+    if(password.length < 8){
+         return false;
+    }
+    
+    // test 2 implementation
+    if(password.indexOf("password") >= 0){
+         return false;
+    }
+
     return true;
 }
